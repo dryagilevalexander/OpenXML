7d82b907d15bd3b42319057edf4da9009fb34d49
Alex Dr <dryagilevalexander@gmail.com>
2023-04-20 00:33:09 +0300

    minor edits

--- OpenXML/Program.cs
+++ OpenXML/Program.cs
@@ -1,9 +1,4 @@
-﻿using DocumentFormat.OpenXml;
-using DocumentFormat.OpenXml.Packaging;
-using DocumentFormat.OpenXml.Wordprocessing;
-using System.Reflection.Metadata;
-using System.Text;
-using OpenXML;
+﻿using OpenXML;
 
 Contragent contragentMain = new Contragent()
 {
